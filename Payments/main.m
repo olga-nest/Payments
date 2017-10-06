f68df01dd405102563808ef0e9cbfb6fb11d295f
olga-nest <olga.w.nesterova@gmail.com>
2017-10-06 14:42:01 -0700

    Grab user's input

--- Payments/main.m
+++ Payments/main.m
@@ -1,17 +1,36 @@
-//
-//  main.m
-//  Payments
-//
-//  Created by Olga on 10/6/17.
-//  Copyright © 2017 Olga. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+
+        
+            
+        int randomDollarValue = arc4random_uniform(1000) + 100;
+            
+        NSLog(@"Thank you for shopping at Acme.com \n Your total today is %d. \n Please select your payment method: \n 1: Paypal, 2: Stripe, 3: Amazon", randomDollarValue);
+        
+        
+        char answerCString[255];
+        
+        fgets(answerCString, 255, stdin);
+        
+        NSString *result = [NSString stringWithCString:answerCString
+                                              encoding:NSUTF8StringEncoding];
+        NSString *parsedResult = [result stringByTrimmingCharactersInSet: [NSCharacterSet whitespaceAndNewlineCharacterSet]];
+        
+        NSInteger choice = [parsedResult integerValue];
+        
+        NSLog(@"Input was %li", (long)choice);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
     return 0;
 }
